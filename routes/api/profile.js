235d294ee352dd688ce16ff6a35380bb7e150e8e
Justin Elliott <Justinelliott11375@gmail.com>
2019-12-27 22:52:13 -0600

    add experience to user profile working

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -160,4 +160,61 @@ router.delete('/', auth, async (req, res) => {
     }
 });
 
+// @route    PUT api/profile/experience
+// @desc     Add experience to profile
+// @access   Public
+router.put('/experience', [ 
+    auth, [
+        validator.check('title', 'Title is require').not().isEmpty(),
+        validator.check('company', 'Company is require').not().isEmpty(),
+        validator.check('from', 'From date is require').not().isEmpty()
+    ]
+], async (req, res) => {
+    const errors = validator.validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newExperience = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.experience.unshift(newExperience);
+
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+
+});
+
+// @route    DELETE api/profile/experience/:exp_id
+// @desc     Delete experience from profile
+// @access   Private
+
+
+
 module.exports = router;
